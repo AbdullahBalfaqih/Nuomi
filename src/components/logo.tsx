adf7defbdccef5ed174dbcd0c82879d03e945dd7
AbdullahBalfaqih <137425906+AbdullahBalfaqih@users.noreply.github.com>
2026-04-14 16:21:34 +0300

    Resize logo component dimensions

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,7 +10,7 @@ export default function Logo({ className }: LogoProps) {
   const logoUrl = "https://res.cloudinary.com/ddznxtb6f/image/upload/v1773617679/_PhotoFixerBot_23-31-36_UTC-removebg-preview_xwt1h8.png";
   
   return (
-      <div className={cn("relative h-[31px] w-[114px]", className)}>
+      <div className={cn("relative h-[62px] w-[228px]", className)}>
         <Image src={logoUrl} alt="NUOMI Logo" fill className="object-contain" />
       </div>
   );

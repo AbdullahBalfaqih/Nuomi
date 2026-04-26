4928e05ed0efaeb6359c6b7f4d183c67b450cdfc
AbdullahBalfaqih <137425906+AbdullahBalfaqih@users.noreply.github.com>
2026-04-26 15:23:06 +0300

    Delete blank Firebase config file
    Remove empty Firebase configuration file.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,1 @@
-// This file is intentionally left blank. 
-// The Firebase configuration will be populated by the backend.
-import { FirebaseOptions } from 'firebase/app';
 
-export const firebaseConfig: FirebaseOptions = {};
